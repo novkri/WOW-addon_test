1228b77b9f3ac57c77c000f032000b5f439030c3
kris <nov.kri@yandex.ru>
2022-02-07 16:54:54 +0700

    playing with buttons

--- Core.lua
+++ Core.lua
@@ -1,5 +1,6 @@
-SLASH_RELOADUI = "/rl" -- quick reload
-SlashCmdList.RELOADUI = ReloadUI
+-- TODO: add ReloadUI function
+-- SLASH_RELOADUI = "/rl" -- quick reload
+-- SlashCmdList.RELOADUI = ReloadUI
 
 SLASH_FRAMESTK1 = "/fs" -- quick access to frame stack
 SlashCmdList.FRAMESTK = function ()
@@ -14,7 +15,24 @@ end
 
 ----------------------------------------------------------
 
-local UIConfig = CreateFrame("Frame", "HelloWorldFrame", UIParent, "BasicFrameTemplateWithInset, MovableTemplate, ResizableTemplate")
+-- Parent frame: 
+local UIConfig = CreateFrame("Frame", "HelloWorldFrame", UIParent, "BasicFrameTemplateWithInset")
 
 UIConfig:SetSize(300, 360)
 UIConfig:SetPoint("CENTER", UIParent, "CENTER")
+
+UIConfig.title = UIConfig:CreateFontString(nil, "OVERLAY")
+UIConfig.title:SetFontObject("GameFontHighlight")
+UIConfig.title:SetPoint("LEFT", UIConfig.TitleBg, "LEFT", 5, 0)
+UIConfig.title:SetText('Hello World')
+
+-- Child frames and regions:
+--BUTTONS
+UIConfig.saveButton = CreateFrame("Button", nil, UIConfig, "GameMenuButtonTemplate")
+UIConfig.saveButton:SetPoint("CENTER", UIConfig, "TOP", 0, 0)
+UIConfig.saveButton.SetSize(140, 40)
+UIConfig.saveButton:SetText("Save")
+UIConfig.saveButton:SetNormalFontObject("GameFontNormalLarge")
+UIConfig.saveButton:SetHighlightFontObject("GameFontHighlightLarge")
+
+UIConfig.saveButton:SetPushedFontObject("Pushed")
---
+++ readme.md
@@ -0,0 +1,2 @@
+just simple HelloWorld
+3.3.5 patch
